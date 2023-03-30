1fe113296b1cef49d10acb3c998d7e83e82ef58f
visor517 <visor517@yandex.ru>
2023-03-30 19:59:32 +0300

    complete

--- run.py
+++ run.py
@@ -1,55 +1,65 @@
-import requests
 import json
+import os
+import requests
+
 from bs4 import BeautifulSoup
-from pprint import pprint
 
 from timezones import TIMEZONE_BY_REGION
 
 
-code = "900"
-url = f"https://www.kody.su/mobile/{code}"
 headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.99 Safari/537.36"}
 
-response = requests.get(url, headers=headers)
-dom = BeautifulSoup(response.text, "html.parser")
+# собираем страницы
+for code in range(900, 1000):
+    url = f"https://www.kody.su/mobile/{code}"
+    response = requests.get(url, headers=headers)
+    dom = BeautifulSoup(response.text, "html.parser")
 
-data_table = dom.find("table", {"class": "tbnum"})
-items = data_table.find_all("tr")
+    if (data_table := dom.find("table", {"class": "tbnum"})) is None:
+        print(f'Нет страницы {url}')
+        continue
 
-result = []
+    items = data_table.find_all("tr")
 
-for item in items[1:100]:
-    b = item.find_all("td")
-    interval, operator, region = [td.getText().strip() for td in item.find_all("td")]
+    result = []
+    for item in items[1:]:
+        # собираем данные объекта
+        b = item.find_all("td")
+        interval, operator, region = [td.getText().strip() for td in item.find_all("td")]
 
-    interval = interval.replace(f"{code}-", "")
-    if len(interval) == 7:
-        interval_from = interval.replace("x", "0")
-        interval_to = interval.replace("x", "9")
-    else:
-        interval_from = interval[:7].replace("x", "0")
-        interval_to = interval[-7:].replace("x", "9")
+        interval = interval.replace(f"{code}-", "")
+        if len(interval) == 7:
+            interval_from = interval.replace("x", "0")
+            interval_to = interval.replace("x", "9")
+        else:
+            interval_from = interval[:7].replace("x", "0")
+            interval_to = interval[-7:].replace("x", "9")
 
-    timezone = TIMEZONE_BY_REGION.get(region)
-    if not TIMEZONE_BY_REGION.get(region):
-        print(region)
+        timezone = TIMEZONE_BY_REGION.get(region)
+        if not TIMEZONE_BY_REGION.get(region):
+            print(f"Новый регион - {region}")
 
-    if region in ["Москва"]:
-        city = region
-        region = None
-    else:
-        city = None
+        if region in ["Москва", "Санкт-Петербург"]:
+            city = region
+            region = None
+        else:
+            city = None
 
-    item_obj = {
-        "code": code,
-        "from": interval_from,
-        "to": interval_to,
-        "operator": operator,
-        "region": region,
-        "city": city,
-        "district": None,
-        "timezone": timezone,
-    }
-    result.append(item_obj)
+        # заполняем объект
+        item_obj = {
+            "code": code,
+            "from": interval_from,
+            "to": interval_to,
+            "operator": operator,
+            "region": region,
+            "city": city,
+            "district": None,
+            "timezone": timezone,
+        }
+        result.append(item_obj)
 
-# pprint(result)
+    # сохранение результатов
+    if not os.path.exists("results"):
+        os.makedirs("results")
+    with open(f'results/DEF-9xx.json', 'w', encoding='UTF-8') as file:
+        file.write(json.dumps(result, ensure_ascii=False))
